c12ba012f687a8f5a65c55a10f7fcec4a799d860
dongjun lee <ddongjunn@naver.com>
2021-07-05 00:09:08 +0900

    Update member.sql

--- src/member.sql
+++ src/member.sql
@@ -12,11 +12,3 @@ CREATE TABLE MEMBER(
     AUTH NUMBER(1) NOT NULL
 );
 
-
-INSERT INTO MEMBER (ID, PWD, NAME, BIRTH, GENDER, EMAIL, ADDR, AUTH)
-VALUES('admin', '1234', '관리자', '00000000', '남자', 'admin@admin.com', '서울시 마포구 백범로23, 3층', '3');
-
-INSERT INTO MEMBER (ID, PWD, NAME, BIRTH, GENDER, EMAIL, ADDR, AUTH)
-VALUES('mem', '1234', '남규', '19931103', '남자', 'skarbsla93@naver.com', '서울시 마포구 와우산로14길19, 3층', '0');
-
-select * from member;
